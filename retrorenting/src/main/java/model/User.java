9b6e8a40a2792fc457b910fbe79b118c0f2c03d8
NestorTur <nestor.tur.ntf@gmail.com>
2024-04-19 14:05:46 +0200

    Recuperamos la estructura original del proyecto debido a una creacion de ramas con una estructura obsoleta

--- retrorenting/src/main/java/model/User.java
+++ retrorenting/src/main/java/model/User.java
@@ -2,7 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.retrorenting.retrorenting.model;
+package model;
 
 
 import com.retrorenting.retrorenting.configuration.db.DbConnect;
