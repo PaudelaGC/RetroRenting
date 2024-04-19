ed474af81356f83af750934281c5515dd46f8b2b
NestorTur <nestor.tur.ntf@gmail.com>
2024-04-19 11:02:56 +0200

    Creada la estructura del projecto

--- retrorenting/src/main/java/model/persist/UserDao.java
+++ retrorenting/src/main/java/model/persist/UserDao.java
@@ -2,12 +2,12 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.retrorenting.retrorenting.model;
+package model.persist;
 
 /**
  *
- * @author 39348
+ * @author Mati
  */
-public class Request {
+public class UserDao {
     
 }
