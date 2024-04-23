b458fd0a7c9a7626ba98d8ad7384427bd9b137a7
NestorTur <nestor.tur.ntf@gmail.com>
2024-04-23 12:03:55 +0200

    Ya esta creado de forma definitiva tanto UsersDao como AddressDao, ahora queda crear las demas de forma funcional

--- retrorenting/src/main/java/model/persist/UsersDao.java
+++ retrorenting/src/main/java/model/persist/UsersDao.java
@@ -8,7 +8,9 @@ import com.retrorenting.retrorenting.configuration.db.DbConnect;
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import model.Address;
 import model.User;
+
 
 /**
  *
@@ -21,32 +23,46 @@ public class UsersDao {
         dbConnect = new DbConnect();
     }
     
-    private User fromResultSet(ResultSet rs) throws SQLException{
-        return new User(
-            rs.getInt("id"),
-            rs.getString("name"),
-            rs.getString("surname"),
-            rs.getString("mail"),
-            rs.getString("password"),
-            rs.getDate("birthdate"),
-            rs.getInt("idAddress")
-        );
-    }
-    
-    public List<User> listUsers(){
-        List<User> result = new ArrayList<>();
-        String query = "SELECT * FROM users;";
-        try (Connection conn = dbConnect.getConnection();
-             Statement st = conn.createStatement();
-             ResultSet rs = st.executeQuery(query)) {
-            while (rs.next()){
-                result.add(fromResultSet(rs));
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
+private User fromResultSet(ResultSet rs) throws SQLException {
+    Address address = new Address(
+        rs.getInt("address_id"), // Asumiendo que los campos de Address se prefijan o se unan correctamente en la consulta SQL
+        rs.getString("street"),
+        rs.getString("number"),
+        rs.getString("block"),
+        rs.getString("door"),
+        rs.getString("floor"),
+        rs.getString("postal_code"),
+        rs.getString("city"),
+        rs.getString("state"),
+        rs.getString("country")
+    );
+    return new User(
+        rs.getInt("id"),
+        rs.getString("name"),
+        rs.getString("surname"),
+        rs.getString("mail"),
+        rs.getString("password"),
+        rs.getDate("birthdate"),
+        address
+    );
+}
+
+public List<User> listUsers() {
+    List<User> result = new ArrayList<>();
+    // Asume una JOIN con la tabla addresses
+    String query = "SELECT users.*, addresses.id as address_id, addresses.street, addresses.number, addresses.block, addresses.door, addresses.floor, addresses.postal_code, addresses.city, addresses.state, addresses.country FROM users JOIN addresses ON users.idAddress = addresses.id;";
+    try (Connection conn = dbConnect.getConnection();
+         Statement st = conn.createStatement();
+         ResultSet rs = st.executeQuery(query)) {
+        while (rs.next()) {
+            result.add(fromResultSet(rs));
         }
-        return result;
+    } catch (SQLException ex) {
+        ex.printStackTrace();
     }
+    return result;
+}
+
     
     public User searchUser(int id) {
         User result = null;
@@ -65,8 +81,11 @@ public class UsersDao {
         return result;
     }
     
-        public boolean addUser(User user) {
-        boolean result = false;
+        public int addUser(User user) {
+        int id = 0;
+        AddressDao addDao = new AddressDao();
+                int addressId = addDao.addAddress(user.getAddress()); // Primero crea la dirección y obtén el ID.
+
         String query = "INSERT INTO users (name, surname, mail, password, birthdate, idAddress) VALUES (?, ?, ?, ?, ?, ?    );";
         try(Connection conn = dbConnect.getConnection();
                 
@@ -83,11 +102,17 @@ public class UsersDao {
             st.setString(3, user.getMail());
             st.setString(4, user.getPassword());
             st.setDate(5, user.getBirthdate());
-            st.setString(6, user.getidAddress());
-            result = st.executeUpdate() > 0;
-        } catch (Exception e) {
-            e.printStackTrace();
-        } return result;          
+            st.setInt(6, addressId);
+            int affectedRows = st.executeUpdate();
+            if (affectedRows > 0){
+                try(ResultSet rs = st.getGeneratedKeys()){
+                    id = rs.getInt(1);
+                }
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+            id = 0;
+        } return id;          
     }
     
     public boolean editUser (User user){
@@ -99,8 +124,9 @@ public class UsersDao {
             st.setString(2, user.getSurname());
             st.setString(3, user.getMail());
             st.setString(4, user.getPassword());
-            st.setDate(5, user.getBirthdate());
-            st.setString(6, user.getidAddress());
+            st.setDate(5, new java.sql.Date(user.getBirthdate().getTime()));
+            st.setInt(6, user.getAddress().getId()); // Usa getId() del objeto Address para obtener el idAddress
+            st.setInt(7, user.getId());
             result = st.executeUpdate() > 0;
         } catch (Exception e) {
             e.printStackTrace();
@@ -118,6 +144,7 @@ public class UsersDao {
             ex.printStackTrace();
         } return result;
     }
+    
     
     
 }
