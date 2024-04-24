ef0762e5a7411cc71274c06db9e09a918dc35b41
NestorTur <nestor.tur.ntf@gmail.com>
2024-04-24 10:45:07 +0200

    Hora de testear estos DAOs añadidos

--- retrorenting/src/main/java/model/persist/AddressDao.java
+++ retrorenting/src/main/java/model/persist/AddressDao.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/*
-*
-* Author Nestor
-*/
 package model.persist;
 
 import com.retrorenting.retrorenting.configuration.db.DbConnect;
@@ -16,119 +7,124 @@ import java.util.List;
 import model.Address;
 
 public class AddressDao {
-
     private final DbConnect dbConnect;
 
     public AddressDao() {
         dbConnect = new DbConnect();
     }
 
-    private Address fromResultSet(ResultSet rs) throws SQLException {
-        return new Address(
-            rs.getInt("id"),
-            rs.getString("street"),
-            rs.getString("number"),
-            rs.getString("block"),
-            rs.getString("door"),
-            rs.getString("floor"),
-            rs.getString("postal_code"),
-            rs.getString("city"),
-            rs.getString("state"),
-            rs.getString("country")
-        );
-    }
-
+    // List all addresses
     public List<Address> listAddresses() {
-        List<Address> result = new ArrayList<>();
+        List<Address> addresses = new ArrayList<>();
         String query = "SELECT * FROM addresses;";
         try (Connection conn = dbConnect.getConnection();
-             Statement st = conn.createStatement();
-             ResultSet rs = st.executeQuery(query)) {
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
             while (rs.next()) {
-                result.add(fromResultSet(rs));
+                Address address = new Address(
+                    rs.getInt("id"),
+                    rs.getString("street"),
+                    rs.getString("number"),
+                    rs.getString("block"),
+                    rs.getString("door"),
+                    rs.getString("floor"),
+                    rs.getString("postal_code"),
+                    rs.getString("city"),
+                    rs.getString("state"),
+                    rs.getString("country")
+                );
+                addresses.add(address);
             }
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
-        return result;
+        return addresses;
     }
 
-    public Address searchAddress(int id) {
-        Address result = null;
+    // Search an address by ID
+    public Address findAddressById(int id) {
+        Address address = null;
         String query = "SELECT * FROM addresses WHERE id = ?;";
         try (Connection conn = dbConnect.getConnection();
-             PreparedStatement st = conn.prepareStatement(query)) {
-            st.setInt(1, id);
-            try (ResultSet rs = st.executeQuery()) {
-                if (rs.next()) {
-                    result = fromResultSet(rs);
-                }
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                address = new Address(
+                    rs.getInt("id"),
+                    rs.getString("street"),
+                    rs.getString("number"),
+                    rs.getString("block"),
+                    rs.getString("door"),
+                    rs.getString("floor"),
+                    rs.getString("postal_code"),
+                    rs.getString("city"),
+                    rs.getString("state"),
+                    rs.getString("country")
+                );
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return address;
+    }
+
+    // Add a new address
+    public boolean addAddress(Address address) {
+        boolean result = false;
+        String query = "INSERT INTO addresses (street, number, block, door, floor, postal_code, city, state, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+        try (Connection conn = dbConnect.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, address.getStreet());
+            stmt.setString(2, address.getNumber());
+            stmt.setString(3, address.getBlock());
+            stmt.setString(4, address.getDoor());
+            stmt.setString(5, address.getFloor());
+            stmt.setString(6, address.getPostalCode());
+            stmt.setString(7, address.getCity());
+            stmt.setString(8, address.getState());
+            stmt.setString(9, address.getCountry());
+            result = stmt.executeUpdate() > 0;
         } catch (SQLException ex) {
             ex.printStackTrace();
         }
         return result;
     }
 
-    public int addAddress(Address address) {
-        int id = 0;
-        String query = "INSERT INTO addresses (street, number, block, door, floor, postal_code, city, state, country) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);";
-        try (Connection conn = dbConnect.getConnection();
-             PreparedStatement st = conn.prepareStatement(query)) {
-            st.setString(1, address.getStreet());
-            st.setString(2, address.getNumber());
-            st.setString(3, address.getBlock());
-            st.setString(4, address.getDoor());
-            st.setString(5, address.getFloor());
-            st.setString(6, address.getPostalCode());
-            st.setString(7, address.getCity());
-            st.setString(8, address.getState());
-            st.setString(9, address.getCountry());
-            int affectedRows = st.executeUpdate();
-            if (affectedRows > 0) {
-                try (ResultSet rs = st.getGeneratedKeys()) {
-                    if (rs.next()) {
-                        id = rs.getInt(1); // Obtiene el primer campo de la clave generada, que debería ser el ID.
-                    }
-                }
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            id = 0; // Resetear a 0 en caso de error.
-        } return id; // Devolver el ID generado o 0 si hubo error.
-    }
-
-    public boolean editAddress(Address address) {
+    // Update an existing address
+    public boolean updateAddress(Address address) {
         boolean result = false;
         String query = "UPDATE addresses SET street = ?, number = ?, block = ?, door = ?, floor = ?, postal_code = ?, city = ?, state = ?, country = ? WHERE id = ?;";
         try (Connection conn = dbConnect.getConnection();
-             PreparedStatement st = conn.prepareStatement(query)) {
-            st.setString(1, address.getStreet());
-            st.setString(2, address.getNumber());
-            st.setString(3, address.getBlock());
-            st.setString(4, address.getDoor());
-            st.setString(5, address.getFloor());
-            st.setString(6, address.getPostalCode());
-            st.setString(7, address.getCity());
-            st.setString(8, address.getState());
-            st.setString(9, address.getCountry());
-            st.setInt(10, address.getId());
-            result = st.executeUpdate() > 0;
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-        }return result;
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, address.getStreet());
+            stmt.setString(2, address.getNumber());
+            stmt.setString(3, address.getBlock());
+            stmt.setString(4, address.getDoor());
+            stmt.setString(5, address.getFloor());
+            stmt.setString(6, address.getPostalCode());
+            stmt.setString(7, address.getCity());
+            stmt.setString(8, address.getState());
+            stmt.setString(9, address.getCountry());
+            stmt.setInt(10, address.getId());
+            result = stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 
+    // Delete an address
     public boolean deleteAddress(int id) {
         boolean result = false;
         String query = "DELETE FROM addresses WHERE id = ?;";
         try (Connection conn = dbConnect.getConnection();
-             PreparedStatement st = conn.prepareStatement(query)) {
-            st.setInt(1, id);
-            result = st.executeUpdate() > 0;
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            result = stmt.executeUpdate() > 0;
         } catch (SQLException ex) {
             ex.printStackTrace();
-        } return result;
-    }                                                                       
+        }
+        return result;
+    }
 }
-
--- retrorenting/src/main/java/model/persist/UsersDao.java
+++ retrorenting/src/main/java/model/persist/UsersDao.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model.persist;
 
 import com.retrorenting.retrorenting.configuration.db.DbConnect; 
@@ -11,140 +7,111 @@ import java.util.List;
 import model.Address;
 import model.User;
 
-
-/**
- *
- * @author Nestor
- */
 public class UsersDao {
     private final DbConnect dbConnect;
     
-    public UsersDao(){
+    public UsersDao() {
         dbConnect = new DbConnect();
     }
-    
-private User fromResultSet(ResultSet rs) throws SQLException {
-    Address address = new Address(
-        rs.getInt("address_id"), // Asumiendo que los campos de Address se prefijan o se unan correctamente en la consulta SQL
-        rs.getString("street"),
-        rs.getString("number"),
-        rs.getString("block"),
-        rs.getString("door"),
-        rs.getString("floor"),
-        rs.getString("postal_code"),
-        rs.getString("city"),
-        rs.getString("state"),
-        rs.getString("country")
-    );
-    return new User(
-        rs.getInt("id"),
-        rs.getString("name"),
-        rs.getString("surname"),
-        rs.getString("mail"),
-        rs.getString("password"),
-        rs.getDate("birthdate"),
-        address
-    );
-}
 
-public List<User> listUsers() {
-    List<User> result = new ArrayList<>();
-    // Asume una JOIN con la tabla addresses
-    String query = "SELECT users.*, addresses.id as address_id, addresses.street, addresses.number, addresses.block, addresses.door, addresses.floor, addresses.postal_code, addresses.city, addresses.state, addresses.country FROM users JOIN addresses ON users.idAddress = addresses.id;";
-    try (Connection conn = dbConnect.getConnection();
-         Statement st = conn.createStatement();
-         ResultSet rs = st.executeQuery(query)) {
-        while (rs.next()) {
-            result.add(fromResultSet(rs));
+    public List<User> listUsers() {
+        List<User> users = new ArrayList<>();
+        String query = "SELECT * FROM users;";
+        try (Connection conn = dbConnect.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(query)) {
+            while (rs.next()) {
+                User user = new User();
+                user.setId(rs.getInt("id"));
+                user.setName(rs.getString("name"));
+                user.setSurname(rs.getString("surname"));
+                user.setMail(rs.getString("mail"));
+                user.setPassword(rs.getString("password"));
+                user.setBirthdate(rs.getDate("birthdate"));
+                Address address = new Address(); // Crea una nueva instancia de Address
+                address.setId(rs.getInt("idAddress")); // Asigna solo el ID del Address
+                user.setAddress(address); // Asigna el objeto Address al usuario
+                users.add(user);
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
         }
-    } catch (SQLException ex) {
-        ex.printStackTrace();
+        return users;
     }
-    return result;
-}
 
-    
-    public User searchUser(int id) {
-        User result = null;
+
+ public User searchUser(int id) {
+        User user = null;
         String query = "SELECT * FROM users WHERE id = ?;";
         try (Connection conn = dbConnect.getConnection();
-             PreparedStatement st = conn.prepareStatement(query)) {
-             st.setInt(1, id);
-             try (ResultSet rs = st.executeQuery()) {
-                if (rs.next()) {
-                    result = fromResultSet(rs);
-                }
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            ResultSet rs = stmt.executeQuery();
+            if (rs.next()) {
+                user = new User();
+                user.setId(rs.getInt("id")); // esto es necesario?
+                user.setName(rs.getString("name"));
+                user.setSurname(rs.getString("surname"));
+                user.setMail(rs.getString("mail"));
+                user.setPassword(rs.getString("password"));
+                user.setBirthdate(rs.getDate("birthdate"));
+                Address address = new Address(); // Crea una nueva instancia de Address
+                address.setId(rs.getInt("idAddress")); // Asigna solo el ID del Address
+                user.setAddress(address); // Asigna el objeto Address al usuario
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
+        return user;
+    }
+
+    public boolean addUser(User user) {
+        boolean result = false;
+        String query = "INSERT INTO users (name, surname, mail, password, birthdate, idAddress) VALUES (?, ?, ?, ?, ?, ?);";
+        try (Connection conn = dbConnect.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getSurname());
+            stmt.setString(3, user.getMail());
+            stmt.setString(4, user.getPassword());
+            stmt.setDate(5, new java.sql.Date(user.getBirthdate().getTime()));
+            stmt.setInt(6, user.getAddress().getId()); // Asignar directamente el idAddress
+            result = stmt.executeUpdate() > 0;
         } catch (SQLException e) {
             e.printStackTrace();
         }
         return result;
     }
-    
-        public int addUser(User user) {
-        int id = 0;
-        AddressDao addDao = new AddressDao();
-                int addressId = addDao.addAddress(user.getAddress()); // Primero crea la dirección y obtén el ID.
 
-        String query = "INSERT INTO users (name, surname, mail, password, birthdate, idAddress) VALUES (?, ?, ?, ?, ?, ?    );";
-        try(Connection conn = dbConnect.getConnection();
-                
-                // insert de address user.adress
-//            addredao dao.add(user.getAddress())    
-                
-                // idaddress -> user
-                
-                // insert de user
-            
-            PreparedStatement st = conn.prepareStatement(query)) {
-            st.setString(1, user.getName());
-            st.setString(2, user.getSurname());
-            st.setString(3, user.getMail());
-            st.setString(4, user.getPassword());
-            st.setDate(5, user.getBirthdate());
-            st.setInt(6, addressId);
-            int affectedRows = st.executeUpdate();
-            if (affectedRows > 0){
-                try(ResultSet rs = st.getGeneratedKeys()){
-                    id = rs.getInt(1);
-                }
-            }
-        } catch (SQLException ex) {
-            ex.printStackTrace();
-            id = 0;
-        } return id;          
-    }
-    
-    public boolean editUser (User user){
+    public boolean editUser(User user) {
         boolean result = false;
-        String query = "UPDATE users SET name = ?, surname = ?, mail = ?, password = ?, birthdate = ?, idAddress = id  WHERE id = ?;";
-        try(Connection conn = dbConnect.getConnection();
-            PreparedStatement st = conn.prepareStatement(query)) {
-            st.setString(1, user.getName());
-            st.setString(2, user.getSurname());
-            st.setString(3, user.getMail());
-            st.setString(4, user.getPassword());
-            st.setDate(5, new java.sql.Date(user.getBirthdate().getTime()));
-            st.setInt(6, user.getAddress().getId()); // Usa getId() del objeto Address para obtener el idAddress
-            st.setInt(7, user.getId());
-            result = st.executeUpdate() > 0;
-        } catch (Exception e) {
+        String query = "UPDATE users SET name = ?, surname = ?, mail = ?, password = ?, birthdate = ?, idAddress = ? WHERE id = ?;";
+        try (Connection conn = dbConnect.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setString(1, user.getName());
+            stmt.setString(2, user.getSurname());
+            stmt.setString(3, user.getMail());
+            stmt.setString(4, user.getPassword());
+            stmt.setDate(5, new java.sql.Date(user.getBirthdate().getTime()));
+            stmt.setInt(6, user.getAddress().getId()); // Asignar directamente el idAddress
+            stmt.setInt(7, user.getId());
+            result = stmt.executeUpdate() > 0;
+        } catch (SQLException e) {
             e.printStackTrace();
-        } return result;          
+        }
+        return result;
     }
-    
-    public boolean deleteAddress (int id){
+
+    public boolean deleteUser(int id) {
         boolean result = false;
         String query = "DELETE FROM users WHERE id = ?;";
         try (Connection conn = dbConnect.getConnection();
-             PreparedStatement st = conn.prepareStatement(query)) {
-            st.setInt(1, id);
-            result = st.executeUpdate() > 0;
+             PreparedStatement stmt = conn.prepareStatement(query)) {
+            stmt.setInt(1, id);
+            result = stmt.executeUpdate() > 0;
         } catch (SQLException ex) {
             ex.printStackTrace();
-        } return result;
+        }
+        return result;
     }
-    
-    
-    
 }
