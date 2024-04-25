a567aa1c1de302cec66bc690d29775da5360af68
NestorTur <nestor.tur.ntf@gmail.com>
2024-04-25 11:10:33 +0200

    La connexion a la DB esta solucionada

--- retrorenting/src/main/java/com/retrorenting/retrorenting/configuration/db/DbConnect.java
+++ retrorenting/src/main/java/com/retrorenting/retrorenting/configuration/db/DbConnect.java
@@ -4,6 +4,7 @@
  */
 package com.retrorenting.retrorenting.configuration.db;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
@@ -13,38 +14,49 @@ import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * Encapsulates data for database connection.
+ */
 public final class DbConnect {
 
-    private static final String PROPERTIES_FILE = "/dbConnection.properties";
-    private static String BD_URL;
-    private static Properties props = new Properties();
+    // Database connection parameters
+    private static final String DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String PROTOCOL = "jdbc:mysql:";
+    private static final String HOST = "localhost";
+    private static final String BD_NAME = "retrorenting";
+    private static final String USER = "root";
+    private static final String PASSWORD = "123456";
+    private static final String OPTIONS = "?autoReconnect=true&useSSL=false";
+    private static final String BD_URL = String.format("%s//%s/%s%s", PROTOCOL, HOST, BD_NAME, OPTIONS);
 
     static {
-        loadConnectionProperties();
-    }
-
-    private static void loadConnectionProperties() {
-        try (InputStream input = DbConnect.class.getResourceAsStream(PROPERTIES_FILE)) {
-            if (input == null) {
-                throw new RuntimeException("Unable to find " + PROPERTIES_FILE);
-            }
-            props.load(input);
-            BD_URL = props.getProperty("jdbc.protocol") +
-                     props.getProperty("jdbc.host") + '/' +
-                     props.getProperty("jdbc.dbname") +
-                     props.getProperty("jdbc.options");
-            Class.forName(props.getProperty("jdbc.driver"));
-        } catch (IOException | ClassNotFoundException ex) {
-            Logger.getLogger(DbConnect.class.getName()).log(Level.SEVERE, "Error loading database properties", ex);
+        try {
+            loadDriver();
+        } catch (ClassNotFoundException e) {
+            Logger.getLogger(DbConnect.class.getName()).log(Level.SEVERE, "Driver not found", e);
+            throw new ExceptionInInitializerError(e);
         }
     }
 
+    /**
+     * Loads the database driver.
+     * @throws ClassNotFoundException if the JDBC driver class is not found.
+     */
+    private static void loadDriver() throws ClassNotFoundException {
+        Class.forName(DRIVER);
+    }
+
+    /**
+     * Gets and returns a connection to the database.
+     * @return Connection to the database.
+     */
     public static Connection getConnection() {
+        Connection conn = null;
         try {
-            return DriverManager.getConnection(BD_URL, props.getProperty("jdbc.user"), props.getProperty("jdbc.password"));
+            conn = DriverManager.getConnection(BD_URL, USER, PASSWORD);
         } catch (SQLException ex) {
             Logger.getLogger(DbConnect.class.getName()).log(Level.SEVERE, "Error getting database connection", ex);
         }
-        return null;
+        return conn;
     }
 }
