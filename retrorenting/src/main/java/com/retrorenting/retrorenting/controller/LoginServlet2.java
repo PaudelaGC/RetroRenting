22506d553ff3e19186059dda4ef891c910cedae9
Paudelagisi <dr.gameglitch@gmail.com>
2024-04-26 11:33:40 +0200

    All links working. New LoginServlet2 in use

--- retrorenting/src/main/java/com/retrorenting/retrorenting/controller/LoginServlet2.java
+++ retrorenting/src/main/java/com/retrorenting/retrorenting/controller/LoginServlet2.java
@@ -6,20 +6,27 @@ package com.retrorenting.retrorenting.controller;
 
 import jakarta.servlet.RequestDispatcher;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
+import model.persist.UsersDao;
 
 /**
  *
  * @author 39348
  */
-@WebServlet(name = "UserProfileServlet", urlPatterns = {"/UserProfileServlet"})
-public class UserProfileServlet extends HttpServlet {
+@WebServlet(name = "LoginServlet2", urlPatterns = {"/LoginServlet2"})
+public class LoginServlet2 extends HttpServlet {
+
+    private UsersDao userDao;
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        userDao = new UsersDao();
+    }
 
     /**
      * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
@@ -32,7 +39,6 @@ public class UserProfileServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
@@ -47,17 +53,11 @@ public class UserProfileServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        HttpSession session = request.getSession();
-        String profile = request.getParameter("profile");
-        if(profile != null && profile.equals("self")){
-            request.setAttribute("profile", profile);
-        }
-        String token = (String) session.getAttribute("token");
-        if (token != null) {
-            response.addHeader("Authorization", "Bearer " + token);
-            response.getWriter().write(token);
-        }
-        RequestDispatcher dispatcher = request.getRequestDispatcher("userProfile.jsp");
+        String user = request.getParameter("usuario_id");
+        String post = request.getParameter("publicacion_id");
+        request.setAttribute("usuario_id", user);
+        request.setAttribute("publicacion_id", post);
+        RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
         dispatcher.forward(request, response);
     }
 
@@ -72,7 +72,37 @@ public class UserProfileServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        String email = request.getParameter("email");
+        String password = request.getParameter("password");
+        Integer id = userDao.loginUser(email, password);
+        String userId = "";
+        if (id == null) {
+            userId = "null";
+        } else {
+            userId = Integer.toString(id);
+        }
+        String user = request.getParameter("usuario_id");
+        String post = request.getParameter("publicacion_id");
+        if (!userId.equals("null")) {
+            TokenService tokenService = new TokenService();
+            String token = tokenService.createToken(userId);
+            response.addHeader("Authorization", "Bearer " + token);
+            response.getWriter().write(token);
+            if (user.length() != 0) {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("userPostProfile.jsp");
+                dispatcher.forward(request, response);
+            } else if (post.length() != 0) {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("paymentForm.jsp");
+                dispatcher.forward(request, response);
+            } else {
+                RequestDispatcher dispatcher = request.getRequestDispatcher("home.jsp");
+                dispatcher.forward(request, response);
+            }
+        } else {
+            request.setAttribute("denied", "denied");
+            RequestDispatcher dispatcher = request.getRequestDispatcher("login.jsp");
+            dispatcher.forward(request, response);
+        }
     }
 
     /**
